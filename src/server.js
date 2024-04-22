37ca11fbbbbc0a81f741d05d92cf8de3d3c1eba4
ibrahimherith <ibrahimherith19@gmail.com>
2024-04-22 22:48:22 +0300

    Routes controllers models

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log("Server Started");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT;
+
+connectDB();
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
